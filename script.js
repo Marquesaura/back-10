ac4c83af37b50d40fa9217ef43449bc4e78b7e77
Laura Marques <161334577+Marquesaura@users.noreply.github.com>
2025-03-12 14:50:28 -0300

    i

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let db = {
     celebrantes: [],
     participantes: [],
-    celebracoes:[]
+    celebracoes: []
 };
 
-function salvarDados(){
+function salvarDados() {
     localStorage.setItem('db', JSON.stringify(db))
 }
-function carregarDados(){
+function carregarDados() {
     const dados = localStorage.getItem('db');
 
-    if (dados){
+    if (dados) {
         db = JSON.parse(dados);
     }
 }
-function adicionarCelebrantes(){
+function adicionarCelebrantes() {
     const nome = document.getElementById("celebranteNome").value;
     const nascimento = document.getElementById("celebranteNascimento").value
     const id = db.celebrantes.length + 1
@@ -23,7 +23,7 @@ function adicionarCelebrantes(){
     listarCelebrantes();
     listarParticipantes();
 }
-function adicionarParticipantes(){
+function adicionarParticipantes() {
     const nome = document.getElementById("participanteNome").value;
     const nascimento = document.getElementById("participanteNascimento").value
     const id = db.celebrantes.length + 1
@@ -32,16 +32,60 @@ function adicionarParticipantes(){
     listarCelebrantes();
     listarParticipantes();
 }
-function adicionarCelebracao(){
+function adicionarCelebracao() {
     const data = document.getElementById("dataCelebracao").value
     const horario = document.getElementById("horarioCelebracao").value
     const id_celebrante = document.getElementById("celebranteCelebracao").value
-    db.participantes.push({id, data, horario, id_celebrante})
+    db.participantes.push({ id, data, horario, id_celebrante })
     salvarDados()
     listarCelebracoes()
 }
-function listarCelebracoes(){
+function listarCelebracoes() {
     let lista = "";
-    db.celebracoes.forEach(c => {const celebrante = db.celebrantes.find(x => x.id == c.id_celebrante)?.nome || 'Desconhecido'; lista +=`<li>${c.data} - ${c.horario}- ${c.celebrante} <button onclick = 'excluirCelebracao(${c.id})'>Excluir</button><li>`;});
+    db.celebracoes.forEach(c => { const celebrante = db.celebrantes.find(x => x.id == c.id_celebrante)?.nome || 'Desconhecido'; lista += `<li>${c.data} - ${c.horario}- ${c.celebrante} <button onclick = 'excluirCelebracao(${c.id})'>Excluir</button><li>`; });
     document.getElementById("listaCelebracao").innerHTML = lista;
 }
+function listarCelebrantes() {
+    let lista = "";
+    db.celebrantes.forEach(c => { lista += `<li>${c.nome} - ${c.nascimento} <button onclick ='excluirCelebrantes(${c.id})>Excluir</button></li>)'`; });
+    document.getElementById("listaCelebracao").innerHTML = lista;
+    carregarCelebrantesDropdown();
+}
+function listarParticipantes() {
+    let lista = "";
+    db.participantes.forEach(p => {
+        lista += `<li>${p.nome} - ${p.nascimento} <button onclick = 'excluirParticipante(${p.id})'>Excluir</button></li>`;
+    });
+    document.getElementById("listaParticipantes").innerHTML = lista;
+}
+function excluirCelebrantes(id) {
+    db.celebrantes = db.celebrantes.filter( c => c.id !== id);
+    salvarDados();
+    listarCelebrantes();
+    listarAniversariantes();
+}
+function excluirParticipante(id){
+    db.participantes = db.participantes.filter( c => c.id !== id);
+    salvarDados();
+    listarCelebrantes();
+    listarAniversariantes();
+}
+function excluirCelebracao(id){
+    db.celebracoes = db.celebracoes.filter( c => c.id !== id);
+    salvarDados();
+    listarCelebrantes();
+    listarAniversariantes();
+}
+function listarAniversariantes(){
+    const hoje = new Date();
+    const diaAtual = hoje.getDate();
+    const mesAtual = hoje.getMonth()+1;
+    let aniversariantes = "";
+}
+window.onload - function(){
+    carregarDados()
+    listarCelebracoes();
+    listarParticipantes();
+    listarCelebrantes();
+    listarAniversariantes();
+};
